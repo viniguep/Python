39db7343bca05edb6c14819ab8dcd2df9a787b05
viniguep <viniguep@gmail.com>
2022-02-28 18:58:57 -0300

    Ultima Aula: 01
    A ultima aula postada por @viniguep foi a aula-01

--- Python/Exercicios/exercicio-01.index.py
+++ Python/Exercicios/exercicio-01.index.py
@@ -1,3 +1,7 @@
-print('Hello world!')
-# ou também 
-print('Olá mundo!')
+# Parte 1
+print("Hello, world!")
+print('Olá, mundo!')
+
+# Parte 2
+nome = input(f'Digite o seu nome: ')
+print(f'Prazer em te conhecer {nome}')
